4e9088b124bb53a984f1fb8fba76d3348ba17ed2
Mike White <mike.wildcats@gmail.com>
2020-01-17 11:17:43 -0500

    Initial commit and bot setup

---
+++ Gatekeeper/Program.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Gatekeeper
+{
+    class Program
+    {
+		private DiscordSocketClient _client;
+		private CommandService _commands;
+
+		public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();	
+
+		public async Task MainAsync()
+		{
+			using (var services = ConfigureServices())
+			{
+				_client = new DiscordSocketClient();
+				var handler = new CommandHandlerService(services);
+
+				_client.Log += Log;
+
+				await _client.LoginAsync(TokenType.Bot,
+					Environment.GetEnvironmentVariable("DiscordToken"));
+				await _client.StartAsync();
+
+				await services.GetRequiredService<CommandHandlerService>().InstallCommandsAsync();
+
+				// Block this task until the program is closed.
+				await Task.Delay(-1);
+			}
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		private ServiceProvider ConfigureServices()
+		{
+			return new ServiceCollection()
+				.AddSingleton<DiscordSocketClient>()
+				.AddSingleton<CommandService>()
+				.AddSingleton<CommandHandlerService>()
+				.BuildServiceProvider();
+		}
+	}
+}
---
+++ Gatekeeper/Services/CommandHandlerService.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatekeeper
+{
+    public class CommandHandlerService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private IServiceProvider _services;
+
+        public CommandHandlerService(IServiceProvider services)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+
+            _commands.CommandExecuted += CommandExecutedAsync;
+            
+            _client.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+                                            services: _services);
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a system message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (!(message.HasStringPrefix("$.", ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            // Create a WebSocket-based command context based on the message
+            var context = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: _services);
+
+            // if (!result.IsSuccess)
+            // await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // command is unspecified when there was a search failure (command not found); we don't care about these errors
+            if (!command.IsSpecified)
+                return;
+
+            // the command was successful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess)
+                return;
+
+            // the command failed, let's notify the user that something happened.
+            await context.Channel.SendMessageAsync($"error: {result}");
+        }
+    }
+}
