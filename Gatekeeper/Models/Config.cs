f84bb04b827bccf6ae8826f747568208a0819523
Mike White <mike.wildcats@gmail.com>
2020-01-24 09:02:15 -0500

    Config model and json data store, update to gitignore next

---
+++ Gatekeeper/Models/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gatekeeper.Models
+{
+    class Config
+    {
+        public int BaseCharReq { get; set; }
+        public int BaseScore { get; set; }
+        public int AdditionalCharsScore { get; set; }
+        public int PromoThreshold { get; set; }
+        public int RequiredWords { get; set; }
+
+        public Config (int baseCharReq, int baseScore, int additionalCharsScore, int promoThreshold, int requiredWords)
+        {
+            BaseCharReq = baseCharReq;
+            BaseScore = baseScore;
+            AdditionalCharsScore = additionalCharsScore;
+            PromoThreshold = promoThreshold;
+            RequiredWords = requiredWords;
+        }
+    }
+}
