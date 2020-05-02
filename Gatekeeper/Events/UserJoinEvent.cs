53da3062751bcd0ed229bf585fdbf86920402beb
Mike White <mike.wildcats@gmail.com>
2020-05-02 12:59:00 -0400

    formatting fix in ping message

--- Gatekeeper/Events/UserJoinEvent.cs
+++ Gatekeeper/Events/UserJoinEvent.cs
@@ -40,7 +40,7 @@ namespace Gatekeeper.Events
                 "**1)** Please visit <https://everneth.com/rules/> and at a minimum read Sections 1, 2, 4, and 7. There are more in the rules but the rest of the sections cover rules for staff and how to change our rules.\n" +
                 "**2)** Go to the <#679141347994894336> channel and use the `!!apply` command.\n" +
                 "**3)** Answer the questions asked by the bot and when ready, submit your application!\n" +
-                "**4)** Once you put your app in, start chatting in our discord! I will begin keeping tabs on you and will move you to Pending once you have met the requirements! (Shouldn't take you but maybe 30 minutes to an hour!)\n **If you have a friend, they can confirm they know you and you skip to pending!**" +
+                "**4)** Once you put your app in, start chatting in our discord! I will begin keeping tabs on you and will move you to Pending once you have met the requirements! (Shouldn't take you but maybe 30 minutes to an hour!)\n **If you have a friend, they can confirm they know you and you skip to pending!**\n" +
                 "**5)** Once you meet requirements, staff will vote on your application in Discord. if approved, you will get changed to Citizen automatically and whitelisted.\n\n" +
                 "**And thats it!** Good luck!\n\n ***Jasper** - Your friendly guild bouncer and welcoming committee*\n\n **PS:** __If you are already whitelisted and still receieved this message, please login to the game and use `/discordsync` and link your account!__");
             }
