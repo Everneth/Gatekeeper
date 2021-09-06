c47fcebae4a9b7ab311189e1e50c895b7a992953
Jordan Wren <jordanmwren@gmail.com>
2021-09-05 21:38:54 -0600

    Created script helper and added jar command module

---
+++ Gatekeeper/Commands/PaperJarModule.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using Gatekeeper.Helpers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Gatekeeper.Commands
+{
+    [Group("jar")]
+    public class PaperJarModule : JasperBase
+    {
+        [Command("update")]
+        [Summary("Takes a specified minecraft version and updates the main/test server jars to the latest build under that version")]
+        public async Task UpdateJar(string minecraftVersion)
+        {
+            string pattern = @"1.\d+.?\d+";
+            if (Regex.IsMatch(minecraftVersion, pattern))
+            {
+                ScriptHelper.Run("/home/servers/volumes/main/process/update-jar.sh " + minecraftVersion);
+                await Context.Channel.SendMessageAsync("Running paper jar update script for minecraft version `" + minecraftVersion + "`.");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("That's not a valid minecraft version.");
+            }
+        }
+    }
+}
---
+++ Gatekeeper/Helpers/ScriptHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Gatekeeper.Helpers
+{
+    public static class ScriptHelper
+    {
+        // Starts a new process by calling via bash to execute a script specified in arguments
+        public static void Run(string arguments)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = arguments };
+            Process proc = new Process() { StartInfo = startInfo };
+            proc.Start();
+        }
+    }
+}
