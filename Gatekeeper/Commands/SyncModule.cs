d767ac1d0fa6a655ced87a914cb93040423ab2a9
Jordan Wren <jordanmwren@gmail.com>
2021-09-21 10:04:27 -0600

    Moved PlayerExists to its own DatabaseHelper class and added sync command

---
+++ Gatekeeper/Commands/SyncModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Gatekeeper.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gatekeeper.Commands
+{
+    public class SyncModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("sync")]
+        [Summary("Checks if you have synced your minecraft account and gives the appropriate roles.")]
+        public async Task CheckSync()
+        {
+            var user = Context.User as SocketGuildUser;
+            var roles = user.Guild.Roles.Where(r => r.Name == "Citizen" || r.Name == "Synced");
+            if (DatabaseHelper.PlayerExists(user))
+            {
+                if (roles.Except(user.Roles).Count() > 0)
+                {
+                    await user.AddRolesAsync(roles);
+                    await ReplyAsync($"{user.Mention}, I have given your roles back.");
+                }
+            }
+            else
+            {
+                await ReplyAsync("You have not synced your minecraft account. Please use `/discordsync` in-game.");
+            }
+        }
+    }
+}
--- Gatekeeper/Events/UserJoinEvent.cs
+++ Gatekeeper/Events/UserJoinEvent.cs
@@ -2,9 +2,8 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Discord.WebSocket;
-using Gatekeeper.Models;
+using Gatekeeper.Helpers;
 using Microsoft.Extensions.DependencyInjection;
-using MySql.Data.MySqlClient;
 
 namespace Gatekeeper.Events
 {
@@ -22,61 +21,28 @@ namespace Gatekeeper.Events
         {
             if (user.IsBot) return;
             
-            //var channel = await user.GetOrCreateDMChannelAsync();
-            var socketChannel = user.Guild.Channels.SingleOrDefault(c => c.Name == "apply");
-            var channel = user.Guild.GetTextChannel(socketChannel.Id);
+            var applyChannel = user.Guild.Channels.SingleOrDefault(c => c.Name == "apply") as SocketTextChannel;
+            var generalChannel = user.Guild.Channels.SingleOrDefault(c => c.Name == "town-square") as SocketTextChannel;
 
-            if (PlayerExists(user))
+            if (DatabaseHelper.PlayerExists(user))
             {
-                var role = user.Guild.Roles.SingleOrDefault(r => r.Name == "Citizen");
-                await user.AddRoleAsync(role);
-                await channel.SendMessageAsync("Hi " + user.Mention + "! \nMy name is Jasper. Welcome ***back*** to the Everneth SMP Discord server!\n\n " +
+                var roles = user.Guild.Roles.Where(r => r.Name == "Citizen" || r.Name == "Synced");
+                await user.AddRolesAsync(roles);
+                
+                // since users with the citizen role do not have access to #apply, we should notify them in town-square
+                await generalChannel.SendMessageAsync("Hi " + user.Mention + "! \nMy name is Jasper. Welcome ***back*** to the Everneth SMP Discord server!\n\n " +
                 "I see that you have already been whitelisted and have Citizen'ed you. Have fun!");
             }
             else
             {
-                await channel.SendMessageAsync("Hi " + user.Mention + "! \nMy name is Jasper. Welcome to the Everneth SMP Discord server!\n " +
+                await applyChannel.SendMessageAsync("Hi " + user.Mention + "! \nMy name is Jasper. Welcome to the Everneth SMP Discord server!\n " +
                 "To get you started I have some information on how to apply using discord! Just follow these simple steps to get your application in.\n\n" +
                 "**1)** Please visit <https://everneth.com/rules/> and at a minimum read Sections 1, 2, 4, and 7. There are more in the rules but the rest of the sections cover rules for staff and how to change our rules.\n" +
-                "**2)** Go to the <#679141347994894336> channel and use the `!!apply` command.\n" +
+                "**2)** Use this channel to run the `!!apply` command.\n" +
                 "**3)** Answer the questions asked by the bot and when ready, submit your application!\n" +
                 "**4)** Once you put your app in, start chatting in our discord! I will begin keeping tabs on you and will move you to Pending once you have met the requirements! (Shouldn't take you but maybe 30 minutes to an hour!)\n **If you have a friend, they can confirm they know you and you skip to pending!**\n" +
                 "**5)** Once you meet requirements, staff will vote on your application in Discord. if approved, you will get changed to Citizen automatically and whitelisted.\n\n" +
                 "**And thats it!** Good luck!\n\n ***Jasper** - Your friendly guild bouncer and welcoming committee*\n\n **PS:** __If you are already whitelisted and still receieved this message, please login to the game and use `/discordsync` and link your account!__");
-            }
-            
-            
-        }
-        private bool PlayerExists(SocketGuildUser user)
-        {
-            string cs = String.Format("server=localhost;userid=admin_emi;password={0};database=admin_emi", "lWw17l8V0W");
-            EMIPlayer player = new EMIPlayer();
-            using (var con = new MySqlConnection(cs))
-            {
-                con.Open();
-
-                var stm = String.Format("SELECT * FROM players WHERE discord_id = {0}", user.Id);
-                var cmd = new MySqlCommand(stm, con);
-
-                using (MySqlDataReader rdr = cmd.ExecuteReader())
-                {
-                    if (rdr.HasRows)
-                    {
-                        while (rdr.Read())
-                        {
-                            player.Id = rdr.GetInt32(0);
-                            player.Name = rdr.GetString(1);
-                            player.UUID = rdr.GetString(2);
-                            //player.MemberId = rdr.GetInt32(3);
-                            player.DiscordId = rdr.GetInt64(4);
-                        }
-                    }
-                }
-
-                if (player.DiscordId == 0 || player == null)
-                    return false;
-                else
-                    return true;
             }
         }
     }
